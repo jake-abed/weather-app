c540e44c5474c36caa44fd56f7899ab95e2dedd1
jake-abed <jakeabed@gmail.com>
2023-09-17 14:03:43 -0700

    Finish functional draft, updateWeatherInfo function

--- scripts/weather.js
+++ scripts/weather.js
@@ -2,6 +2,9 @@
 
 const API_KEY = '1175470757064246b83173445231409';
 
+
+let cOrF = 'F';
+
 //State object to control UI elements.
 //Has the option to register a listener that updates CSS 
 let loadingState = {
@@ -31,35 +34,38 @@ const getWeatherData = async (location) => {
 }
 
 const parseWeatherData = (weatherData) => {
-	const today = weatherData.forecast.forecastday[0];
-	const tomorrow = weatherData.forecast.forecastday[1];
-	const afterTomorrow = weatherData.forecast.forecastday[2];
+	try {
+		const today = weatherData.forecast.forecastday[0];
+		const tomorrow = weatherData.forecast.forecastday[1];
+		const overmorrow = weatherData.forecast.forecastday[2];
 
-	const mapDayForecast = (currentDay) => {
-	return {
-		avgTempC: currentDay.day.avgtemp_c,
-		avgTempF: currentDay.day.avgtemp_f,
-		maxTempC: currentDay.day.maxtemp_c,
-		maxTempF: currentDay.day.maxtemp_f,
-		minTempC: currentDay.day.mintemp_c,
-		minTempF: currentDay.day.mintemp_f,
-		chanceOfRain: currentDay.day.daily_chance_of_rain,
-		chanceOfSnow: currentDay.day.daily_chance_of_snow,
-		condition: currentDay.day.condition.text
-	}
-	}
-
-	return {
-	forecast: {
-		today: mapDayForecast(today),
-		tomorrow: mapDayForecast(tomorrow),
-		afterTomorrow: mapDayForecast(afterTomorrow)            
-	},
-	location: {
-		country: weatherData.location.country,
-		name: weatherData.location.name,
-		region: weatherData.location.region
-	}
+		const mapDayForecast = (currentDay) => {
+			return {
+				avgTempC: currentDay.day.avgtemp_c,
+				avgTempF: currentDay.day.avgtemp_f,
+				maxTempC: currentDay.day.maxtemp_c,
+				maxTempF: currentDay.day.maxtemp_f,
+				minTempC: currentDay.day.mintemp_c,
+				minTempF: currentDay.day.mintemp_f,
+				chanceOfRain: currentDay.day.daily_chance_of_rain,
+				chanceOfSnow: currentDay.day.daily_chance_of_snow,
+				condition: currentDay.day.condition.text
+			}
+		}
+		return {
+			forecast: {
+				today: mapDayForecast(today),
+				tomorrow: mapDayForecast(tomorrow),
+				overmorrow: mapDayForecast(overmorrow)            
+			},
+			location: {
+				country: weatherData.location.country,
+				name: weatherData.location.name,
+				region: weatherData.location.region
+			}
+		}
+	} catch (err) {
+		console.error(err);
 	}
 }
 
@@ -67,7 +73,39 @@ const parseWeatherData = (weatherData) => {
 const locationInput = document.getElementById('location');
 const submitButton = document.querySelector('.submit-button');
 const loadDisplay = document.querySelector('.load-display')
-const weather = document.querySelector('.weather');
+const weather = document.querySelector('.weather-info');
+
+const updateWeatherInfo = () => {
+	if (!weatherData) return console.error('Weather Data not available.');
+	const location = document.querySelector('.city');
+	const country = document.querySelector('.country');
+	const HIGH_TEMP = ' .high-temp';
+	const LOW_TEMP = ' .low-temp';
+	const AVG_TEMP = ' .avg-temp';
+	const RAIN_CHANCE = ' .rain-chance';
+
+	location.innerText = weatherData.location.name + ', '
+	country.innerText = weatherData.location.country;
+
+	const updateDayNode = (daySelector) => {
+		const dayProp = daySelector.replace('.', '');
+		const highTempNode = daySelector + HIGH_TEMP;
+		const lowTempNode = daySelector + LOW_TEMP;
+		const avgTempNode = daySelector + AVG_TEMP;
+		const rainNode = daySelector + RAIN_CHANCE;
+
+		document.querySelector(highTempNode).innerText = weatherData.forecast[dayProp]['maxTemp' + cOrF] + '° ' + cOrF;
+		document.querySelector(lowTempNode).innerText = weatherData.forecast[dayProp]['minTemp' + cOrF] + '° ' + cOrF;
+		document.querySelector(avgTempNode).innerText = weatherData.forecast[dayProp]['avgTemp' + cOrF] + '° ' + cOrF;
+		document.querySelector(rainNode).innerText = weatherData.forecast[dayProp].chanceOfRain + '%'
+	}
+	
+	updateDayNode('.today');
+	updateDayNode('.tomorrow');
+	updateDayNode('.overmorrow');
+
+	return;
+}
 
 let forecast = {
 	tomorrow : {
@@ -98,14 +136,15 @@ submitButton.addEventListener('click', async (e) => {
 		alert('Please provide a location.');
 		return;
 	}
-	if (state === 'loading') {
-		return;
-	}
+	if (state === 'loading') return;
 	if (state === '' || state === 'loaded') {
 		loadingState.state = 'loading';
-		weather.classList.toggle('active', true);
+		weather.classList.remove('active');
 		const data = await getWeatherData(locationInput.value);
 		weatherData = parseWeatherData(data);
-		return loadingState.state = 'loaded';
+		updateWeatherInfo();
+		loadingState.state = 'loaded';
+		weather.classList.add('active');
+		return;
 	}
 });
